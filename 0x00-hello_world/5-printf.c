da30093ce0b66f8fd01e5e35db44207400fad4ec
amanuel <amanuelnobel10@gmail.com>
2021-06-17 05:11:11 -0700

    Using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -prints the phrase in printf
+ * main -prints the phrase with proper grammer, but the outcome is a piece of art,
  * return : 0 if exited properly ,not 0 if not
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
