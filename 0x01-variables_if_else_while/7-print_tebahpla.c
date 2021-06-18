350847071eb67306c9b422084265e8b16b9edafc
amanuel <amanuelnobel10@gmail.com>
2021-06-18 13:35:15 -0700

    Revese alphabet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints lower case alphabets 
- * in reverse,followed by new line
+ * main - print the lowercase alphabets in reverse,
+ * followed by new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char ch = 'z';
+
 	for ( ; ch >= 'a' ; ch--)
+	{
 		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
