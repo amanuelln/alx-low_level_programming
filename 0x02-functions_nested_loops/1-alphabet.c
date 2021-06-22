22c9fde53001c715970f2c810b3567f77769c2f3
amanuel <amanuelnobel10@gmail.com>
2021-06-22 04:04:48 -0700

    Printing alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - print the alphabet in
+ * lowercase letters
+ * Return: Always 0 (Succeses)
+ **/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
