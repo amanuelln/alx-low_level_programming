e85546fefe2cca28bd2e33c59c2365c23177cd4b
amanuel <amanuelnobel10@gmail.com>
2021-06-22 02:54:24 -0700

    Holberton print

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main -prints holberton followed
+ * by a new line
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	_putchar('h');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
