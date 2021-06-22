a5b7703ed200bec68de128184c00ab45adfa5aa1
amanuel <amanuelnobel10@gmail.com>
2021-06-22 05:24:23 -0700

    Check alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - check for alphabet character
+ * @c: character to be checked
+ * return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+}
