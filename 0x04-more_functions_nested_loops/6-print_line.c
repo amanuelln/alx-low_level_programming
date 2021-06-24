dc311d10f022c04c2afba68f22ea639bd4e0b23e
amanuel <amanuelnobel10@gmail.com>
2021-06-24 05:08:53 -0700

    draw stright line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a line
+ * @n: number of _ that should be printed
+ */
+void print_line(int n);
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
