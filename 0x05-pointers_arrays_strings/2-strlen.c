e601ce612c11385035f42f145fb583c33bed39c5
amanuel <amanuelnobel10@gmail.com>
2021-06-28 11:24:46 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns te length of string
+ * @s: string length to print
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (int = 0; s[i] != '\0'; i++)
+	return (i);
+}
