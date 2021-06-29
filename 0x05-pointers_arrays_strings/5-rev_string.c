9da752e1f0f1e3cd6bde42d9c2fa0b882ee546e1
amanuel <amanuelnobel10@gmail.com>
2021-06-29 09:55:43 -0700

    Revese a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: to be reversed
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len, leng;
+
+	len = 0;
+	leng = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	leng = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[leng];
+		s[leng--] = tmp;
+	}
+}
