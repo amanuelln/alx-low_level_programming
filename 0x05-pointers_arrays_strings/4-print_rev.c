f26e5048f3a7a53c2f22bf31f20ef49d00eeb463
amanuel <amanuelnobel10@gmail.com>
2021-06-29 02:16:34 -0700

    two at once hope it works

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * then new line
+ * @s: string print
+ */
+void print_rev(char *s)
+{
+	int i, j, k;
+
+	i = 0;
+
+	 while (s[i] != '\0')
+	 {
+		 i++;
+	 }
+	 k = i;
+
+	 for (j = k - 1; j >= 0; j--)
+	 {
+		 _putchar(s[j]);
+	 }
+	 _putchar('\n')
+}
