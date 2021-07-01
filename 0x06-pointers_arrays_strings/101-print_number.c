3d30e26c1f6fb9ebb472af5418003fe82bd516aa
amanuel <amanuelnobel10@gmail.com>
2021-07-01 03:10:15 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,13 +8,14 @@ void print_number(int n)
 {
 	unsigned int n;
 
-	if (n < 0)
-		n1 = -n;
+	if (n < 0){
+	n1 = -n;
 	_putchar('-');
+	}
 	else
 		n1 = n;
 	if (n1 / 10)
 		print_number(n1 / 10);
 
-	_puychar((n1 % 10 + '0');
+	_putchar((n1 % 10 + '0');
 }
