227a4244d555bd223cdec629bd7b1abc68e39c97
Amanuel <amanuelnobel10@gmail.com>
2021-09-29 04:24:12 -0700

    changes

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints file name
- *
- * Return - 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("%s\n"__FILE__);
-	return(0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
