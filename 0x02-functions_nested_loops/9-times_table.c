f158e85bbcc8ada374612e175d9028424ceb9b10
Amanuel <amanuelnobel10@gmail.com>
2021-10-19 17:11:19 +0000

    new changes

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * * times_table - Print the nine times table
+ * */
+void times_table(void)
+{
+	int i;
+	int j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; e < 10; e++)
+		{
+			if (j == 0)
+			{
+				_putchar('0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				if ((i * j) < 10)
+					_putchar(' ');
+				else
+					_putchar(i * j / 10 + '0');
+				_putchar(i * j % 10 + '0');
+				if (e == 9)
+				{
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+	}
+}
+
