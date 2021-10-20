4efa33b7566f7b3e33c60750ec4091a2a4128f7e
Amanuel <amanuelnobel10@gmail.com>
2021-10-20 05:52:26 +0000

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * * main - prints _putchar
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i = 0;
 	char s[] = "_putchar\n";
+
 	while (s[i])
 	{
 		_putchar (s[i]);
