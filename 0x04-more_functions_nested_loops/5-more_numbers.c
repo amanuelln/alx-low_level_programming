f221a1641da0eb6f090c618c621a8cf2f8905b51
Amanuel <amanuelnobel10@gmail.com>
2021-10-21 16:53:43 +0000

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * more_numbers - print 0..14 ten times
  */
@@ -9,16 +9,15 @@ void more_numbers(void)
 	i = 0;
 	while (i < 10)
 	{
-		n = 0;
-		while (n <= 14)
+		j = 0;
+		while (j <= 14)
 		{
-			if (n >= 10)
-				_putchar((n / 10) + '0');
-			_putchar((j % 10) + '0');
+			if (j >= 10)
+				putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 			j++;
 		}
-		_putchar('\n');
+		putchar('\n');
 		i++;
 	}
 }
-
