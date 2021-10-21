7d043e2ba8e10af44be4ec4e3c9a93624574b0c1
Amanuel <amanuelnobel10@gmail.com>
2021-10-21 16:56:22 +0000

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 		while (j <= 14)
 		{
 			if (j >= 10)
-				putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			j++;
 		}
-		putchar('\n');
+		_putchar('\n');
 		i++;
 	}
 }
