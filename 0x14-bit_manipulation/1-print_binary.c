7042682fd90670df31467e2c74f3f8d184261b6c
amanuel <amanuelnobel10@gmail.com>
2021-12-02 20:35:11 +0000

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,26 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_binary - prints a binary number using bit shifting and masks
- * @n: number to print
- *
- * Return: always void
+ * print_binary - prints the binary representation of a number
+ * @n: parameter
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 32768;
-	char rel_flag = 0;
+	int i, count = 0;
+	unsigned long int current;
 
-	while (mask > 0)
+	for (i = 63; i >= 0; i--)
 	{
-		if ((n & mask) == 0 && (rel_flag || mask == 0x01))
-			_putchar('0');
-		else if ((n & mask) != 0)
+		current = n >> i;
+
+		if (current & 1)
 		{
 			_putchar('1');
-			rel_flag = 1;
+			count++;
 		}
-		mask = mask >> 1;
+		else if (count)
+			_putchar('0');
 	}
+	if (!count)
+		_putchar('0');
 }
-
